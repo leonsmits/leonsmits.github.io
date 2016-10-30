1b60f953d6b655ca04705a1da89f583700f8cc76
leonsmits <leonsmollie@gmail.com>
2016-10-30 22:45:54 +0100

    ds

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ import traceback
 
 app = Flask(__name__)
 
-TUTORIAL_REPOSITORY = 'http://leonsmits.github.io/src/ttl/'
+TUTORIAL_REPOSITORY = 'http://leonsmits.github.io/'
 
 @app.route('/')
 def first_page():
